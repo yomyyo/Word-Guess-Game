8424095f44f6976f9185a447ed0e4f7066dfa5ec
Sammy Tang <samtang226@gmail.com>
2019-03-01 19:23:43 -0800

    javascript now working, html now working, css now working, updated readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Word-Guess-Game
+
+This project is a word guessing game with a theme to the korean pop boy band BTS.
+Just start the game by pressing any letter, if you can fill in all of the blanks, you win!
+If you run out of chances, you lose.
+Have Fun!!!
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,22 @@
-var guessingWords = ["butterfly", "fakelove", "dna", "danger", "justoneday"];
-
+//Global Variables
+var guessingWords = ["butterfly", "fire", "dna", "danger", "idol", "serendipity", "dope"];
 var randomNum = Math.floor(Math.random() * guessingWords.length);
-
-var remainingGuesses = 12;
-
+var remainingGuesses = 5;
 var wins = 0;
-
 var lettersGuessed = [];
-
 var guessedCorrectly = loadGuessedCorrectly(guessingWords, [], randomNum);
 
+//Dom variables
 var pageUnderScore = document.getElementById("underscores");
+var pageGuessedWrong = document.getElementById("guessedWrong");
+var pageWordBox = document.getElementById("wordBox");
 
+//Dom manipulation
+pageGuessedWrong.innerHTML = remainingGuesses  + " Guesses Remaining";
+pageWordBox.innerHTML = lettersGuessed.join(" ");
+pageUnderScore.innerHTML = guessedCorrectly.join(" ");
 
+//function that loads an array with the correct amount of underscores
 function loadGuessedCorrectly(wordArray, underArray, randomNumber){
     for(var i = 0; i < wordArray[randomNumber].length; i++){
         underArray.push('_');
@@ -20,6 +24,7 @@ function loadGuessedCorrectly(wordArray, underArray, randomNumber){
     return underArray;
 }
 
+//function to check whether a character is inside of a string
 function insideString(charInput, str){
     var charArray = str.split('');
     for(var i = 0; i < charArray.length; i++){
@@ -30,6 +35,7 @@ function insideString(charInput, str){
     return false;
 }
 
+//function to check whether a character is inside of an array
 function insideChar(charInput, charArray){
     for(var i = 0; i < charArray.length; i++){
         if(charInput == charArray[i]){
@@ -39,6 +45,7 @@ function insideChar(charInput, charArray){
     return false;
 }
 
+//finds all occurrences of a letter inside of a word
 function indexOfAll(word, letter){
     var indexes = [];
     var wordArray = word.split('');
@@ -50,39 +57,49 @@ function indexOfAll(word, letter){
     return indexes;
 }
 
-function startGame(){
+//main funtion, used to play the game
+function main(){
     document.onkeyup = function(event){ 
         var letter = event.key;
+
+        //check to see if a correct letter has already been guessed
         if(!insideChar(letter, guessedCorrectly)){
+
+            //check to see if the letter is inside selected word, if so, add it to array guessedCorrectly
             if(insideString(letter, guessingWords[randomNum])){
                 var curr = indexOfAll(guessingWords[randomNum], letter);
                 for(var i = 0; i < curr.length; i++){
                     guessedCorrectly[curr[i]] = letter;
                     pageUnderScore.innerHTML = guessedCorrectly.join(" ");
                 }
-                console.log(guessedCorrectly);
             }
         }
         
+        //check to see if a wrong letter has already been guessed
         if(!insideChar(letter, lettersGuessed)){
+
+            //if the letter is wrong, add it to the lettersGuessed and decrement number of guesses
             if(!insideString(letter, guessingWords[randomNum])){
                 lettersGuessed.push(letter);
+                pageWordBox.innerHTML = lettersGuessed.join(" ");
                 remainingGuesses--;
+                pageGuessedWrong.innerHTML = remainingGuesses + " guesses remaining";
                 console.log(lettersGuessed);
             }
         }
 
+        //if user wins, reload the page
         if(guessedCorrectly.join("") == guessingWords[randomNum]){
             location.reload();
             alert("you win!");
         }
 
+        //if the user loses, reload the page.
         if(remainingGuesses == 0){
             location.reload();
             alert("you lose");
         }
     }
 }
-pageUnderScore.innerHTML = guessedCorrectly.join(" ");
 
 startGame();
